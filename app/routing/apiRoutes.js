d126aca2c61b16b829007cbd97788fe27a673081
adkahane <adam.kahane.nj@gmail.com>
2018-02-26 22:01:18 -0800

    post to api. working on logic to find best match

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -8,7 +8,27 @@ module.exports = function(app) {
   
   // Push a friend object to the friendArray
   app.post("/api/friends", function(req, res) {
+    var difArray = [];
+    var totalDif;
+    var highestDif = 0;
+    var bestMatch;
+    console.log(req.body);
+    console.log(friends);
+    for(var i = 0; i < friends.length; i++) {
+      for(var c = 0; c < friends[i].scores.length; c++) {
+        difArray.push(Math.abs(friends[i].scores[c]-req.body.scores[c]));
+      }
+      for(var p = 0; p < difArray.length; p++) {
+        totalDif += difArray[p];
+      }
+      if(totalDif > highestDif) {
+        highestDif = totalDif;
+        bestMatch = friends[i];
+      }
+    }
+
     friends.push(req.body);
-    res.json(true);
+    //logic
+    res.json(bestMatch);
   });
 };
