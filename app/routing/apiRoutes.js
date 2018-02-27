f6931b56c19db9f3884812b80b295a942e108b0b
adkahane <adam.kahane.nj@gmail.com>
2018-02-26 22:51:08 -0800

    logic and best match working. create modal

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -8,27 +8,25 @@ module.exports = function(app) {
   
   // Push a friend object to the friendArray
   app.post("/api/friends", function(req, res) {
-    var difArray = [];
-    var totalDif;
-    var highestDif = 0;
+    
+    var lowestDif = 1000;
     var bestMatch;
-    console.log(req.body);
-    console.log(friends);
     for(var i = 0; i < friends.length; i++) {
+      var totalDif = 0;
+      var difArray = [];
       for(var c = 0; c < friends[i].scores.length; c++) {
-        difArray.push(Math.abs(friends[i].scores[c]-req.body.scores[c]));
+        difArray.push(Math.abs(parseInt(friends[i].scores[c]-req.body.scores[c])));
       }
       for(var p = 0; p < difArray.length; p++) {
         totalDif += difArray[p];
       }
-      if(totalDif > highestDif) {
-        highestDif = totalDif;
+      if(totalDif < lowestDif) {
+        lowestDif = totalDif;
         bestMatch = friends[i];
       }
     }
 
     friends.push(req.body);
-    //logic
     res.json(bestMatch);
   });
 };
